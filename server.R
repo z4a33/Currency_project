588d67819e15406a604c366bf9d0ac2dbeeea495
Emil Zygała <z4a3.lol@gmail.com>
2022-06-14 10:41:37 +0200

    Pliki do shiny

--- server.R
+++ server.R
@@ -3,16 +3,7 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
-  output[["first_plot"]] = renderPlot({
-    one_var_plot = ggplot(surv, aes_string(x = input[["one_var"]])) +
-      theme_bw() +
-      xlab(input[["one_var"]])
-    if (is.numeric(surv[[input[["one_var"]]]])) {
-      one_var_plot + geom_density()
-    } else {
-      one_var_plot + geom_bar()
-    }
-  })
+
 
 
 })
