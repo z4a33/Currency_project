14c24d2bcc165ed33fd60a4a9d6cd2b8103b06d9
unknown <kulbinskim@gmail.com>
2022-06-22 16:12:55 +0200

    nowa wersja

--- actual_prices.R
+++ actual_prices.R
@@ -1,5 +1,6 @@
 install.packages("RJSONIO")
-
+install.packages("data.table")
+install.packages("plyr")
 library(plyr)
 library(RJSONIO)
 library(data.table)
@@ -10,10 +11,10 @@ actual_prices <- function(waluty) {
   path <- "http://api.nbp.pl/api/exchangerates/tables/a/"
   df  <- ldply(fromJSON(path), data.frame)
   df = df[4:length(df)]
-  l = length(df)/3
+  iter = length(df)/3
   waluta = c()
   wartosc = c()
-  for (i in 1:l) {
+  for (i in 1:iter) {
     if (df[3*i - 1] %in% waluty) {
       waluta <- c(waluta, df[3*i - 1])
       wartosc <- c(wartosc, df[3*i])
@@ -21,11 +22,13 @@ actual_prices <- function(waluty) {
   }
   
   dane <- data.frame("waluta" = as.character(c(waluta)),
-                     "wartosc" = as.double(c(wartosc)))
+                     "wartosc" = as.double(c(wartosc))) 
   
-  ggplot(dane, aes(x = waluta, y = wartosc)) +
-    geom_col()
+  ggplot(dane, mapping = aes(x = waluta, y = wartosc)) +
+    geom_bar(fill = "gold",stat = "identity") +
+    theme(legend.position = "none") + 
+    geom_text(aes(label= wartosc), vjust=-0.3, size=5)
   
 }
-
 actual_prices(c("THB","AUD","HKD","USD","CAD","NZD"))
+## test
