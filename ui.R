134595e6716886aed608f88fba855db00e8c4ddb
iza2210 <iza2210@wp.pl>
2022-06-22 23:50:54 +0200

    Final

--- ui.R
+++ ui.R
@@ -1,6 +1,89 @@
+
+
 shinyUI(fluidPage(
   
+  
   tabsetPanel(
+    
+    tabPanel(
+      titlePanel("Porównaj wykresy walut"),
+      actionButton("go1", "Go"),
+      selectInput(
+        'compar_cur',
+        'Porównaj wykresy walut',
+        all_names[2],
+        selected = "EUR",
+        multiple = TRUE,
+        selectize = TRUE,
+        width = NULL,
+        size = NULL),
+      
+      plotOutput("first_plot"),
+      
+      dateInput(
+        'compar_start',
+        "Start date",
+        value = "2021-01-01",
+        min = NULL,
+        max = NULL,
+        format = "yyyy-mm-dd",
+        startview = "month",
+        weekstart = 0,
+        language = "en",
+        width = NULL,
+        autoclose = TRUE,
+        datesdisabled = NULL,
+        daysofweekdisabled = NULL
+      ),
+      
+      dateInput(
+        'compar_end',
+        "End date",
+        value = NULL,
+        min = NULL,
+        max = NULL,
+        format = "yyyy-mm-dd",
+        startview = "month",
+        weekstart = 0,
+        language = "en",
+        width = NULL,
+        autoclose = TRUE,
+        datesdisabled = NULL,
+        daysofweekdisabled = NULL
+      )
+    ),
+    
+    tabPanel(
+      titlePanel("Czy rośnie"),
+      actionButton("go2", "Go"),
+      selectInput(
+        'is_increasing',
+        'czy rośnie',
+        all_names[2],
+        selected = "EUR",
+        multiple = TRUE,
+        selectize = TRUE,
+        width = NULL,
+        size = NULL),
+      
+      dataTableOutput("table_of_move")
+      
+    ),
+    
+    tabPanel(
+      titlePanel("Aktualne ceny w PLN"),
+      h4(paste("Dzisiaj jest", as.character(Sys.Date()), sep = " ")),
+      checkboxGroupInput(inputId = "waluty",
+                         label = "Wybierz waluty",
+                         inline = TRUE,
+                         selected = c("USD", "EUR"),
+                         c("THB", "USD", "	AUD", "HKD", "CAD", 	
+                           "NZD", 	"SGD", 'EUR', "	HUF", 	
+                           "CHF", "GBP", "UAH", "JPY", "CZK", "DKK", "	ISK", 	
+                           "NOK")),
+      actionButton(inputId ="click",label = "Zobacz wykres"),
+      plotOutput("wykres")
+    ),
     
     tabPanel(
       titlePanel("Konwerter walutowy"),
@@ -46,9 +129,12 @@ shinyUI(fluidPage(
         ),
       ),
       mainPanel(
-          h1("Wynik"),
-          verbatimTextOutput("txtout")
+        h1("Wynik"),
+        verbatimTextOutput("txtout")
+      )
+      
     )
     
-  )
-)))
+    
+    
+  )))
